21ac5f0b8cf1cf8e9fbff5b04ee72eb81291a89e
Balázs Úr <urbalazs@gmail.com>
2016-10-16 21:28:48 +0200

    Hungarian translation

---
+++ Kernel/Language/hu_ImportExportDynamicFields.pm
@@ -0,0 +1,31 @@
+# --
+# Kernel/Language/hu_ImportExportDynamicFields.pm - the Hungarian translation for ImportExportDynamicFields
+# Copyright (C) 2011-2016 Perl-Services, http://www.perl-services.de
+# Copyright (C) 2016 Balázs Úr, http://www.otrs-megoldasok.hu
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::hu_ImportExportDynamicFields;
+
+use strict;
+use warnings;
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    my $Lang = $Self->{Translation};
+
+    return if ref $Lang ne 'HASH';
+
+    # Kernel/Config/Files/ImportExportDynamicFields.xml
+    $Lang->{'Override existing dynamic fields when a field with an existing name is imported.'} =
+        'Meglévő dinamikus mezők felülírása, ha egy létező névvel rendelkező mező kerül importálásra.';
+
+    return 1;
+}
+
+1;
